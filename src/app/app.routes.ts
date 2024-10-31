805cd5ca682a1c20afd0fedf8c567a732f435374
MrNizzy <nizzy0128@gmail.com>
2024-10-31 15:03:47 -0500

    Add: home component with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { environment } from '../environments/environment.development';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: `Inicio - ${environment.appName}`,
+  },
+];
