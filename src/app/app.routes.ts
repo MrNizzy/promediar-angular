f692fb60771d4e73252ed116187d3dba2c91b844
MrNizzy <nizzy0128@gmail.com>
2024-11-21 11:55:25 -0500

    Add: include provideAnimationsAsync in app configuration and update environment import path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { environment } from '../environments/environment.development';
+import { environment } from '@environments/environment.development';
 
 export const routes: Routes = [
   {
